95b48f1e5813081bb7f1c0054bdd4abbb97baa9c
Swetony <swetonyancelmo@gmail.com>
2025-08-02 17:03:58 -0300

    finalizacao do projeto

--- src/components/Condicao/Condicao.jsx
+++ src/components/Condicao/Condicao.jsx
@@ -1,33 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import ClimaImage from '../../assets/clima.png';
 import './condicao.css';
 
-export default function Condicao(){
+export default function Condicao() {
+  const [localizacao, setLocalizacao] = useState('');
+  const [condicao, setCondicao] = useState(null);
+
+  useEffect(() => {
+    async function fetchApi() {
+      if (!localizacao) {
+        setCondicao(null);
+        return;
+      }
+
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${localizacao}&appid=d9b3558166b6c7898e47f5b2f35f966a&lang=pt_br&units=metric`
+        );
+        const data = await response.json();
+
+        if (response.ok) {
+          const infos = {
+            temperatura: Math.round(data.main.temp),
+            local: data.name,
+            icone: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+          };
+          setCondicao(infos);
+        } else {
+          setCondicao('Localização não encontrada');
+        }
+      } catch (error) {
+        console.error('Erro ao tentar buscar dados do clima: ', error);
+        setCondicao('Erro ao buscar dados.');
+      }
+    }
+
+    fetchApi();
+  }, [localizacao]);
+
   return (
     <>
       <h1>Condições do Tempo</h1>
 
-      <form>
-        <input 
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          value={localizacao}
+          onChange={(e) => setLocalizacao(e.target.value)}
           type="text"
           placeholder="Digite a localização..."
         />
         <button>Pesquisar</button>
       </form>
-      <div className='condicao-contain'>
-        <div className='city-contain'>
-          <span>Limoeiro</span>
-          <span>45 Graus</span>
-        </div>
 
-        <div className='grau-contain'>
-          <img src="" />
-        </div>
+      <div>
+        {condicao && typeof condicao === 'object' ? (
+          <div className="condicao-contain">
+            <div className="city-contain">
+              <span>{condicao.local}</span>
+              <span>{condicao.temperatura}°C</span>
+            </div>
+
+            <div className="grau-contain">
+              <img src={condicao.icone} alt="Ícone do clima" />
+            </div>
+          </div>
+        ) : condicao ? (
+          <p>{condicao}</p>
+        ) : (
+          <div style={{ textAlign: 'center' }}>
+            <img style={{ width: '200px' }} src={ClimaImage} />
+          </div>
+        )}
       </div>
     </>
-  )
+  );
 }
-
-// #1A1F30
-// #222845
-// #8F5CFF
-// #181C2A
-// #AA72FF
